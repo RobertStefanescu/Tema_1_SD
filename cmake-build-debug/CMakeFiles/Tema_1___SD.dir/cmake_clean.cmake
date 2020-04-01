7d25945c844f71a3101febe623bb2d2359efad55
robertstefanescu <robertstefanescu1@icloud.com>
2020-04-01 03:35:45 +0300

    .

---
+++ cmake-build-debug/CMakeFiles/Tema_1___SD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema_1___SD.dir/main.cpp.o"
+  "Tema_1___SD"
+  "Tema_1___SD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema_1___SD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
